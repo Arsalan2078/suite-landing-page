13bfb9e158ad4245bdda378ee23255e85c18af8a
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-26 22:59:49 -0500

    starter files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from "./App.module.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <div className={css.app} data-text-preset="3">
+            <button data-text-preset="6">Request Beta Access</button>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <h1 data-text-preset="1">
+                A <b>super solution</b> for your <b>business.</b>
+            </h1>
+
+            <p>
+                Our marketing and sales automations help you scale your outreach
+                to get more leads for your company.
+            </p>
+
+            <button data-text-preset="6">Request Beta Access</button>
+
+            <ul>
+                <li>
+                    <div data-text-preset="2">
+                        <b>2K+</b>
+                    </div>
+                    <div data-text-preset="7">Companies</div>
+                </li>
+                <li>
+                    <div data-text-preset="2">
+                        <b>8</b>
+                    </div>
+                    <div data-text-preset="7">Languages</div>
+                </li>
+                <li>
+                    <div data-text-preset="2">
+                        <b>1.2M</b>
+                    </div>
+                    <div data-text-preset="7">Leads</div>
+                </li>
+            </ul>
+
+            <h2 data-text-preset="2">
+                It just <b>works.</b>
+            </h2>
+
+            <figure>
+                <blockquote>
+                    I really like how it is an all-in-one solution that handle
+                    many of the tasks that you would normally need separate
+                    tools to do the same job. This thing is a miracle worker.
+                </blockquote>
+
+                <figcaption>
+                    <div data-text-preset="4">Jeremy Robinson</div>
+                    <div data-text-preset="7">CMO, Fylo</div>
+                </figcaption>
+            </figure>
+
+            <div data-text-preset="8">Copyright - Suite</div>
+        </div>
+    );
 }
-
-export default App
